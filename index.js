47d970d412c336149ec1b7765c4eb73ff87a869f
Asanga <asanga.nicholas@yahoo.com>
2019-10-28 12:19:24 +0100

    express server

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ const express = require('express')
 
 
 const app = express()
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 4000
+
 app.listen(port, () => console.log(`server is listening on ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'))
