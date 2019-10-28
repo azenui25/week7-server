20f84aa740090e569b69adf1406ab86dd4e6b895
soniapathak <sonia.soni04@gmail.com>
2019-10-28 15:08:25 +0100

    added User folder with model and router

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
-               
 
+const bodyParser = require('body-parser')
+const bodyParserMiddleWare = bodyParser.json()
 
-
-
-
-
-
+const db = require('./db')
+const UserRouter = require('./User/router');
 const app = express()
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 4000
+
+
 app.listen(port, () => console.log(`server is listening on ${port}!`))
+app.get('/test', (req, res) => res.send('hello world'))
+
+app.use(bodyParserMiddleWare)
+app.use(UserRouter)
+
+
+//
