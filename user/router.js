d3369566eb96f7b87a973119a49f7f4b37760a8d
Asanga <asanga.nicholas@yahoo.com>
2019-10-29 11:55:26 +0100

    handled error

--- user/router.js
+++ user/router.js
@@ -6,26 +6,27 @@ const router = new Router();
 
 
 router.get('/user', (req, res, next) => {
-    User.findAll()
-      .then(user => {
-        res.send(user);
-      })
-      .catch(next);
-  });
-
-
-router.post('/user', (req,res) => {
-    console.log("create user")
-    User.create({
-        email: req.body.email,
-        password :  bcrypt.hashSync(req.body.password, 10)
+  User.findAll()
+    .then(user => {
+      res.send(user);
     })
-.then(user => {
-    res
-    .status(201)
-    .send({message: "user created sucessfully"})
-.catch(err => next(err))
-})
+    .catch(next);
+});
+
+
+router.post('/user', (req, res) => {
+  console.log("create user")
+  User.create({
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  })
+    .then(user => {
+      res
+        .status(201)
+        .send({ message: "user created sucessfully" })
+    })
+    .catch(err => next(err))
+
 })
 
 module.exports = router
